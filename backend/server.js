9425a2e571ed2edafceb866db0b401d8bedfe5b5
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-22 03:20:40 +0700

    feat: created a route for fetching movies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/movie', movieRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}`);
