c24b23b4621b852f94f29107e839685664a23720
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-27 02:45:06 +0700

    feat: navigated user to sign in and sign up pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,37 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/home/HomePage.jsx';
-import SignInPage from './pages/SignInPage.jsx';
-import SignUpPage from './pages/SignUpPage.jsx';
-import Footer from './components/Footer.jsx';
+import HomePage from './pages/home/HomePage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import Footer from './components/Footer';
 import { Toaster } from 'react-hot-toast';
+import { useAuthStore } from './store/authUser';
+import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
+import { Loader } from 'lucide-react';
 
 export default function App() {
+  const { user, isCheckingAuth, authCheck } = useAuthStore()
+
+  useEffect(() => {
+    authCheck()
+  }, [authCheck])
+
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen">
+        <div className="flex justify-center items-center bg-black h-full">
+          <Loader className="animate-spin text-red-600 size-10"/>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/signin' element={<SignInPage />} />
+        <Route path='/signup' element={user ? <Navigate to={"/"} /> : <SignUpPage />} />
+        <Route path='/signin' element={user ? <Navigate to={"/"} /> : <SignInPage />} />
       </Routes>
       <Footer />
       <Toaster />
