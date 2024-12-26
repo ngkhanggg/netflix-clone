e3c33c3ece9507a199affd5597bef9571168fdf9
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-27 01:18:22 +0700

    feat: displayed footer under every route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage.jsx';
 import SignInPage from './pages/SignInPage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
+import Footer from './components/Footer.jsx';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path='/' element={<HomePage />} />
-      <Route path='/signup' element={<SignUpPage />} />
-      <Route path='/signin' element={<SignInPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/signin' element={<SignInPage />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
