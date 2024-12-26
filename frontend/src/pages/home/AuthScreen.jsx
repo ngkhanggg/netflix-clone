652f6980637ba91f4f7b9f374044d5af90040577
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-27 02:46:41 +0700

    feat: included the email to the url and direct the user to sign up page when clicking on get started button

--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { ChevronRight } from 'lucide-react'
 
 function AuthScreen() {
     const [email, setEmail] = React.useState('')
+    const navigate = useNavigate()
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        navigate("/signup?email=" + email)
+    }
+
     return (
         <div className="hero-bg relative">
             {/* Navbar */}
@@ -27,7 +33,7 @@ function AuthScreen() {
                 <p className="mb-4">
                     Ready to watch? Enter your email to create or restart your membership.
                 </p>
-                <form className="flex flex-col md:flex-row gap-4 w-1/2">
+                <form className="flex flex-col md:flex-row gap-4 w-1/2" onSubmit={handleFormSubmit}>
                     <input 
                         type="email"
                         placeholder="Email address"
