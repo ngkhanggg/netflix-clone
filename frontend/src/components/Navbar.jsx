56acfc35f9e862516db49559326a7d01c3543f52
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-28 22:09:24 +0700

    feat: created a navigation bar to switch between movies and tv shows (responsive), show user avatar, and search icon

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Search, LogOut, Menu } from 'lucide-react'
+import { useAuthStore } from '../store/authUser'
+import { useContentStore } from '../store/content'
+
+const Navbar = () => {
+    const [ isMobileMenuOpen, setIsMenuMobileOpen ] = useState(false)
+    const toggleMobileMenu = () => setIsMenuMobileOpen(!isMobileMenuOpen)
+    const { user, signout } = useAuthStore()
+    const { setContentType } = useContentStore()
+
+    return (
+        <header className="max-w-6xl max-auto flex flex-wrap items-center justify-between p-4 h-20">
+            <div className="flex items-center gap-10 z-50">
+                <Link to="/">
+                    <img src="/netflix-logo.png" alt="Netflix logo" className="w-32 sm:w-40"/>
+                </Link>
+
+                {/* Desktop navbar items */}
+                <div className="hidden sm:flex gap-2 items-center">
+                    <Link to="/" className="hover:underline" onClick={() => setContentType("Movie")}>
+                        Movies
+                    </Link>
+                    <Link to="/" className="hover:underline" onClick={() => setContentType("TV")}>
+                        TV Shows
+                    </Link>
+                    <Link to="/" className="hover:underline">
+                        Search History
+                    </Link>
+                </div>
+            </div>
+
+            <div className="flex gap-2 items-center z-50">
+                <Link to={"/search"} className="hover:underline">
+                    <Search className="size-6 cursor-pointer"/>
+                </Link>
+                <img src={user.image} alt="Avatar" className="h-8 rounded cursor-pointer"/>
+                <LogOut className="size-6 cursor-pointer" onClick={signout}/>
+                <div className="sm:hidden">
+                    <Menu className="size-6 cursor-pointer" onClick={toggleMobileMenu}/>
+                </div>
+            </div>
+
+            {/* Mobile navbar items */}
+            {isMobileMenuOpen && (
+                <div className="w-full sm:hidden mt-4 z-50 bg-black border rounded border-gray-800">
+                    <Link
+                        to="/"
+                        className="block hover:underline p-2"
+                        onClick={toggleMobileMenu}
+                    >
+                        Movies
+                    </Link>
+                    <Link
+                        to="/"
+                        className="block hover:underline p-2"
+                        onClick={toggleMobileMenu}
+                    >
+                        TV Shows
+                    </Link>
+                    <Link
+                        to="/history"
+                        className="block hover:underline p-2"
+                        onClick={toggleMobileMenu}
+                    >
+                        Search History
+                    </Link>
+                </div>
+            )}
+        </header>
+    )
+}
+
+export default Navbar
