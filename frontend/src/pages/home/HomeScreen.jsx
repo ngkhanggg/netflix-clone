353898803cee500b7b09ce7b2de3936ae7b4abf6
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-28 23:34:11 +0700

    fix: wrapped return with braces

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -7,14 +7,16 @@ import { ORIGINAL_IMG_BASE_URL } from '../../utils/constants'
 const HomeScreen = () => {
   const { trendingContent } = useGetTrendingContent()
 
-  if (!trendingContent) return (
-    <div className="h-screen text-white relative">
-      <Navbar/>
-      <div
-        className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer"
-      />
-    </div>
-  )
+  if (!trendingContent) {
+    return (
+      <div className="h-screen text-white relative">
+        <Navbar/>
+        <div
+          className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer"
+        />
+      </div>
+    )
+  }
 
   return (
     <div className="relative h-screen text-white">
