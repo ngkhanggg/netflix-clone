15b34cfa276adaae67fc8feaafc4510f879c85ed
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-28 23:41:17 +0700

    fix: moved div to a fragment

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,11 +1,22 @@
 import { Info, Play } from 'lucide-react'
-import Navbar from '../../components/Navbar'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+
+import Navbar from '../../components/Navbar'
+import MovieSlider from '../../components/MovieSlider'
+
 import useGetTrendingContent from '../../hooks/useGetTrendingContent'
-import { ORIGINAL_IMG_BASE_URL } from '../../utils/constants'
+
+import { useContentStore } from '../../store/content'
+
+import { ORIGINAL_IMG_BASE_URL, MOVIE_CATEGORIES, TV_CATEGORIES } from '../../utils/constants'
 
 const HomeScreen = () => {
   const { trendingContent } = useGetTrendingContent()
+  const { contentType } = useContentStore()
+  const [ imageLoading, setImageLoading ] = useState(true)
+
+  const categories = contentType === "movie" ? MOVIE_CATEGORIES : TV_CATEGORIES
 
   if (!trendingContent) {
     return (
@@ -19,45 +30,55 @@ const HomeScreen = () => {
   }
 
   return (
-    <div className="relative h-screen text-white">
-      <Navbar />
-      <img src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path} alt="Image" className="absolute top-0 left-0 w-full h-full object-cover -z-50"/>
-      <div className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
-        aria-hidden="true"
-      />
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
-        <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10"/>
-        <div className="max-w-2xl">
-          <h1 className="mt-4 text-6xl font-extrabold text-balance">
-            {trendingContent?.title || trendingContent?.name}
-          </h1>
-          <p className="mt-2 text-lg">
-            {trendingContent?.release_date?.split("-")[0] || trendingContent?.first_air_date?.split("-")[0]}{" "}
-            |{" "}
-            {trendingContent?.adult ? "18+" : "PG-13"}{" "}
-          </p>
-          <p className="mt-4 text-lg">
-            {trendingContent?.overview.length > 200 ? trendingContent?.overview.slice(0, 200) + "..." : trendingContent?.overview}
-          </p>
-        </div>
-        <div className="flex mt-8">
-          <Link
-            to={`/watch/${trendingContent?.id}`}
-            className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center"
-          >
-            <Play className="size-6 mr-2 fill-black"/>
-            Play
-          </Link>
-          <Link
-            to={`/watch/${trendingContent?.id}`}
-            className="bg-gray-500/70 hover:bg-gray/500 text-white font-bold py-2 px-4 rounded flex items-center"
-          >
-            <Info className="size-6 mr-2"/>
-            More Info
-          </Link>
+    <>
+      {/* Trending */}
+      <div className="relative h-screen text-white">
+        <Navbar />
+        <img
+          src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
+          alt="Image"
+          className="absolute top-0 left-0 w-full h-full object-cover -z-50"
+          onLoad={() => {
+            setImageLoading(false)
+          }}
+        />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/50 -z-50"
+          aria-hidden="true"
+        />
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center px-8 md:px-16 lg:px-32">
+          <div className="bg-gradient-to-b from-black via-transparent to-transparent absolute w-full h-full top-0 left-0 -z-10"/>
+          <div className="max-w-2xl">
+            <h1 className="mt-4 text-6xl font-extrabold text-balance">
+              {trendingContent?.title || trendingContent?.name}
+            </h1>
+            <p className="mt-2 text-lg">
+              {trendingContent?.release_date?.split("-")[0] || trendingContent?.first_air_date?.split("-")[0]}{" "}
+              |{" "}
+              {trendingContent?.adult ? "18+" : "PG-13"}{" "}
+            </p>
+            <p className="mt-4 text-lg">
+              {trendingContent?.overview.length > 200 ? trendingContent?.overview.slice(0, 200) + "..." : trendingContent?.overview}
+            </p>
+          </div>
+          <div className="flex mt-8">
+            <Link
+              to={`/watch/${trendingContent?.id}`}
+              className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex items-center"
+            >
+              <Play className="size-6 mr-2 fill-black"/>
+              Play
+            </Link>
+            <Link
+              to={`/watch/${trendingContent?.id}`}
+              className="bg-gray-500/70 hover:bg-gray/500 text-white font-bold py-2 px-4 rounded flex items-center"
+            >
+              <Info className="size-6 mr-2"/>
+              More Info
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
