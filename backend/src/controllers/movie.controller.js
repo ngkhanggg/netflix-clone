ca21ea144a85cdcc59c0e7786241596ea8a50e6e
Khang Nguyen Chu <nguyenkhangchu2004@gmail.com>
2024-12-29 18:49:36 +0700

    fix: changed a key name in the response

--- backend/src/controllers/movie.controller.js
+++ backend/src/controllers/movie.controller.js
@@ -40,7 +40,7 @@ export async function getSimilarMovies(req, res) {
     const { id } = req.params;
     try {
         const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`);
-        res.status(200).json({success: true, content: data.results});
+        res.status(200).json({success: true, similar: data.results});
     } catch (error) {
         res.status(500).json({ success: false, message: 'Internal server error' });
     }
